fcf373a9db2276ee9e127daf343325eff4b34953
karthik raghunathan <kraghunathan@nile-global.com>
2020-05-19 12:49:16 +0530

    refactor 1

--- samples/game.py
+++ samples/game.py
@@ -7,11 +7,13 @@ throw_die = lambda : random.randint(1,6)
 class Board:
     def __init__(self, snakes, ladders, max =100):
         self.max = 100
-        self.snakes = []
-        self.ladders = []
+        self.snakes = snakes
+        self.ladders = ladders
 
     def get_pos(self,x):
+        print(x,self.snakes)
         for i in self.snakes:
+            print(i)
             if i[0] == x:
                 print( "sliding down from",x, "to", i[1])
                 return i[1]
@@ -50,7 +52,7 @@ class Game:
             print( player,  " got " ,jump, "and is now at ", self.player_pos)
 
 
-players =   [1,2,3,4]
+players =   [1]
 snakes =    [ [10,4], [30,4], [40,2], [64,20] ]
 ladders =   [ [2,20], [3,30], [10,90], [5,30], [4,40] ]
 max = 100
